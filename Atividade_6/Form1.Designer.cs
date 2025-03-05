eb7fe30ce7840ece3a6e8705d36a32ec10abde3c
Lucas Eduardo <sardelarilucas@gmail.com>
2025-03-05 20:29:22 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade_6/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Atividade_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_n1 = new System.Windows.Forms.TextBox();
+            this.txt_n2 = new System.Windows.Forms.TextBox();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.lbl_resultado = new System.Windows.Forms.Label();
+            this.cbo_operacao = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculadora Simples";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Número 1 :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(34, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Número 2 :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(34, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Operação :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(52, 277);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Resultado :";
+            // 
+            // txt_n1
+            // 
+            this.txt_n1.Location = new System.Drawing.Point(124, 70);
+            this.txt_n1.Name = "txt_n1";
+            this.txt_n1.Size = new System.Drawing.Size(100, 20);
+            this.txt_n1.TabIndex = 5;
+            // 
+            // txt_n2
+            // 
+            this.txt_n2.Location = new System.Drawing.Point(124, 115);
+            this.txt_n2.Name = "txt_n2";
+            this.txt_n2.Size = new System.Drawing.Size(100, 20);
+            this.txt_n2.TabIndex = 6;
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_calcular.Location = new System.Drawing.Point(107, 207);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(84, 31);
+            this.btn_calcular.TabIndex = 8;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // lbl_resultado
+            // 
+            this.lbl_resultado.AutoSize = true;
+            this.lbl_resultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_resultado.Location = new System.Drawing.Point(144, 277);
+            this.lbl_resultado.Name = "lbl_resultado";
+            this.lbl_resultado.Size = new System.Drawing.Size(15, 16);
+            this.lbl_resultado.TabIndex = 9;
+            this.lbl_resultado.Text = "1";
+            // 
+            // cbo_operacao
+            // 
+            this.cbo_operacao.FormattingEnabled = true;
+            this.cbo_operacao.Items.AddRange(new object[] {
+            "1 - Adição",
+            "2 - Subtração",
+            "3 - Multiplicação",
+            "4 - Divisão"});
+            this.cbo_operacao.Location = new System.Drawing.Point(124, 161);
+            this.cbo_operacao.Name = "cbo_operacao";
+            this.cbo_operacao.Size = new System.Drawing.Size(121, 21);
+            this.cbo_operacao.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 324);
+            this.Controls.Add(this.cbo_operacao);
+            this.Controls.Add(this.lbl_resultado);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.txt_n2);
+            this.Controls.Add(this.txt_n1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_n1;
+        private System.Windows.Forms.TextBox txt_n2;
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.Label lbl_resultado;
+        private System.Windows.Forms.ComboBox cbo_operacao;
+    }
+}
+
---
+++ Atividade_6/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade_6
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            double numero1, numero2, resultado = 0;
+
+            if (cbo_operacao.SelectedItem == null)
+            {
+                MessageBox.Show("Selecione uma operação antes de calcular.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string operacao = cbo_operacao.SelectedItem?.ToString();
+
+
+            // Verifica se os valores digitados são números válidos
+            if (double.TryParse(txt_n1.Text, out numero1) && double.TryParse(txt_n2.Text, out numero2))
+            {
+                switch (operacao)
+                {
+                    case "1 - Adição":
+                        resultado = numero1 + numero2;
+                        break;
+                    case "2 - Subtração":
+                        resultado = numero1 - numero2;
+                        break;
+                    case "3 - Multiplicação":
+                        resultado = numero1 * numero2;
+                        break;
+                    case "4 - Divisão":
+                        if (numero2 != 0)
+                        {
+                            resultado = numero1 / numero2;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Erro: Divisão por zero não é permitida.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+                        break;
+                    default:
+                        MessageBox.Show("Selecione uma operação válida.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                }
+
+                // Exibe o resultado formatado com duas casas decimais
+                lbl_resultado.Text = $"Resultado: {resultado:F2}";
+            }
+            else
+            {
+                MessageBox.Show("Por favor, insira números válidos.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
